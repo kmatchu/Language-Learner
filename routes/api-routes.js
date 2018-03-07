92770b98ddc2efb1533c9a2e4c13a82bd050232a
natraj-s <natraj.subramanian@gmail.com>
2018-03-07 14:36:00 -0600

    Connects to the database and retrieves data back in JSON

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,38 +8,40 @@
 // Requiring our Todo model
 var db = require("../models");
 
+// console.log(db);
+
 // Routes
 // =============================================================
 module.exports = function(app) {
 
   // Get route for retrieving a single user
-  app.get("/api/posts/:username", function(req, res) {
-    db.Login.findOne({
-      where: {
-        username: req.params.username
-      }
-    })
-    .then(function(dbPost) {
-      res.json(dbPost);
-    });
-  });
+  // app.get("/api/posts/:username", function(req, res) {
+  //   db.Login.findOne({
+  //     where: {
+  //       username: req.params.username
+  //     }
+  //   })
+  //   .then(function(dbPost) {
+  //     res.json(dbPost);
+  //   });
+  // });
 
   app.get("/api/french", function(req, res) {
-    db.Word.findAll({}).then(function(dbWord) {
+    db.French.findAll({}).then(function(dbWord) {
       res.json(dbWord);
     })
-  })
+  });
 
   // POST route for saving a new user
-  app.post("/api/posts", function(req, res) {
-    console.log(req.body);
-    db.Login.create({
-      username: req.body.username,
-      password: req.body.password,
-    })
-    .then(function(dbPost) {
-      res.json(dbPost);
-    });
-  });
+  // app.post("/api/posts", function(req, res) {
+  //   console.log(req.body);
+  //   db.Login.create({
+  //     username: req.body.username,
+  //     password: req.body.password,
+  //   })
+  //   .then(function(dbPost) {
+  //     res.json(dbPost);
+  //   });
+  // });
 
 };
