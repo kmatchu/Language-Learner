ee3e869129a06995241c09861a72a7bbfaf9897f
Codi Steinborn <codisteinborn@dhcp-10-102-249-252.wireless.northwestern.private>
2018-03-07 11:16:38 -0600

    server running

--- server.js
+++ server.js
@@ -6,6 +6,8 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var Sequelize = require("sequelize");
+var sequelize = require("./config/config.json");
 
 // Sets up the Express App
 // =============================================================
@@ -13,7 +15,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-var db = require("./models");
+var db = require("./models/login.js");
 
 // Sets up the Express app to handle data parsing
 // parse application/x-www-form-urlencoded
@@ -31,8 +33,8 @@ require("./routes/html-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+// db.Sequelize.sync().then(function(){
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });
+// });
