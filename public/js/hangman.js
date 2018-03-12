cda3e008482a4fd76bdadcc3179a562fca88f97b
Codi Steinborn <codisteinborn@Codis-MacBook-Pro.local>
2018-03-12 11:49:41 -0500

    hm style and descrip

--- public/js/hangman.js
+++ public/js/hangman.js
@@ -4,14 +4,58 @@ var winWordRand = winWordsArr[Math.floor(Math.random() * winWordsArr.length)];
 var winWordLetArr = winWordRand.split("");
 var uArr = (winWordLetArr.fill(" _ ", 0));
 var replace = wordDisplay.innerText = uArr.join(" ");
-
 var lives = 7;
 var wins = 0;
 var losses = 0;
 var allGuess = [];
 var isMatch = false;
 
+function hideLinks() {
+    // USER DATA exists in localstorage
+    if (localStorage.getItem("user") !== null) {
+        $(".signuplink").addClass("hidden");
+        $(".loginlink").addClass("hidden");
+        $(".guestlink").addClass("hidden");
+        $(".welcomemsg").removeClass("hidden");
 
+        username = localStorage.getItem("user");
+        $("#userInfo").text(username);
+    }
+    else {
+        $(".welcomemsg").addClass("hidden");
+        $(".signuplink").removeClass("hidden");
+        $(".loginlink").removeClass("hidden");
+        $(".guestlink").removeClass("hidden");
+    }
+}
+
+hideLinks();
+
+var yandex = function(){
+var language = sessionStorage.getItem("lang");
+   queryURL = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180305T185504Z.d82e099867176c62.926d780dfe710515cb00f16a16c48bd887c06819&%20&text=" + winWordRand + "&lang=" + language + "-en";
+   $.ajax({
+       url: queryURL,
+       type: "GET",
+       dataType: "json",
+   }).then(function (response) {
+       if(response.text[0] !== winWordRand){
+       $("#lookup").text("Translation: " + response.text[0])
+       console.log(response.text[0])
+       }
+   }).catch(function (response){
+      $("#lookup").text("No translation found")
+   });
+};
+yandex();
+
+var render = function(){
+$("#guessesRemain").text(lives);
+$("#win").text(wins);
+$("#loss").text(losses);
+$("#guessedLetters").text(allGuess);
+}
+render();
 
 var begin = function (event) {
     var guess = (event.key).toLowerCase();
@@ -49,33 +93,9 @@ var begin = function (event) {
             replace = wordDisplay.innerText = uArr.join(" ");
         }
     }
+render();
+yandex();
 
-    $("#guessesRemain").text(lives);
-    $("#win").text(wins);
-    $("#loss").text(losses);
-    $("#guessedLetters").text(allGuess);
-    // This onclick function takes the user input and runs our ajax call to Yandex's API to find a translation to english for it
-//  $("#wordSearchBtn").on("click", function () {
-    // event.preventDefault();
-    // $("#lookup").text("");
-   var language = sessionStorage.getItem("lang");
-   queryURL = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180305T185504Z.d82e099867176c62.926d780dfe710515cb00f16a16c48bd887c06819&%20&text=" + winWordRand + "&lang=" + language + "-en";
-   $.ajax({
-       url: queryURL,
-       type: "GET",
-       dataType: "json",
-   }).then(function (response) {
-       if(response.text[0] !== winWordRand){
-       $("#lookup").text("Translation: " + response.text[0])
-       console.log(response.text[0])
-       }
-    //    else{
-    //       $("#lookup").text("No translation found for: " + winWordRand)
-    //    }
-   }).catch(function (response){
-      $("#lookup").text("No translation found")
-   })
-// });
 }
 $("body").on("keyup", begin);
 
