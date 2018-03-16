4006d446fdb0abc88b8ae1fad2b5bf63dbeca6bb
Codi Steinborn <codisteinborn@Codis-MacBook-Pro.local>
2018-03-15 23:24:37 -0500

    jumble js commenting

--- public/js/home.js
+++ public/js/home.js
@@ -6,14 +6,14 @@ $(document).ready(function () {
                     "consectetur a erat nam at lectus urna duis. Pellentesque sit amet porttitor eget dolor morbi non."
 
     var langDescr = {
-       "french": "French description goes here. " + loremIpsum,
-        "spanish": "Spanish description goes here. " + loremIpsum,
-        "german": "German description goes here. " + loremIpsum,
-        "italian": "Italian description goes here. " + loremIpsum,
-        "afrikaans": "Afrikaans description goes here. " + loremIpsum,
-        "portugese": "Portugese description goes here. " + loremIpsum,
-        "hungarian": "Hungarian description goes here. " + loremIpsum,
-        "dutch": "Dutch description goes here. " + loremIpsum
+       "french": "French is a Romance language of the Indo-European family. It is the official language in 29 countries across five different continents, and is estimated to have about 76 million native speakers.",
+        "spanish": "The Spanish language, also called the Castilian language, is a Western Romance language that originated in the Castile region of Spain and today has hundreds of millions of native speakers around the world. It is usually considered the world's second-most spoken native language.",
+        "german": "German is a West Germanic language that is mainly spoken in Central Europe. One of the major languages of the world, German is the first language of about 95 million people worldwide and the most widely spoken native language in the European Union.",
+        "italian": "Italian is a romance language. It is the third most widely spoken first language in the European Union with 69 million native speakers and it is spoken as a second language by 16 million EU citizens.",
+        // "afrikaans": "Afrikaans description goes here. " + loremIpsum,
+        // "portugese": "Portugese description goes here. " + loremIpsum,
+        // "hungarian": "Hungarian description goes here. " + loremIpsum,
+        // "dutch": "Dutch description goes here. " + loremIpsum
     };
 
 var stateFun = (function () {
