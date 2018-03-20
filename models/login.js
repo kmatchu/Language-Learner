5d928968817556accd4b7f6cfdd592a7d2de96ad
natraj-s <natraj.subramanian@gmail.com>
2018-03-20 18:02:22 -0500

    Login model changed. Carousel caption boxes reduced in size

--- models/login.js
+++ models/login.js
@@ -5,23 +5,55 @@ module.exports = function(sequelize, Sequelize) {
 
 // Creates a "Login" model that matches up with DB
 var Login = sequelize.define("Login", {
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
+  id: {
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER
   },
+
   username: {
     type: Sequelize.STRING,
     allowNull: false,
     validate: {
-      len: [1]
+      len: [3],
+      isAlphanumeric: true
     }
   },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    }
+  },
+  
   password: {
     type: Sequelize.STRING,
     allowNull: false,
     validate: {
-      len: [1]
+      len: [3]
     }
+  },
+
+  firstname: {
+    type: Sequelize.STRING
+  },
+
+  lastname: {
+    type: Sequelize.STRING
+  },
+
+  progress: {
+    type: Sequelize.STRING
+  },
+
+  profilepic: {
+    type: Sequelize.STRING
+  },
+
+  resettoken: {
+    type: Sequelize.STRING
   }
 }, {
     timestamps: false
